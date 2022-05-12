574632c5cc6cdff1d689a44f826299692ead4d14
rockyliu0418 <tjblizzard@sina.com>
2022-05-12 14:20:02 +0800

    15:00/12/05/2022

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,20 @@
+/*
+============================================================================
+ Name : HelloWorld.cpp
+ Author :
+ Version :
+ Description : GMAS C++ project source file
+============================================================================
+*/
+
+#include "HelloWorld.h"
+#include <iostream>
+#include "MMC_Definitions.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+	return 0;
+}
